926c520ceac9a08bfbcc7cfe34d5bb0f13af8d01
Lasuch <stylizedstickman@gmail.com>
2023-09-12 12:40:25 +0200

    Initial commit

---
+++ src/register_types.h
@@ -0,0 +1,7 @@
+#ifndef CELLULAR_AUTOMATA_REGISTER_TYPES_H
+#define CELLULAR_AUTOMATA_REGISTER_TYPES_H
+
+void initialize_gdextension_types();
+void uninitialize_gdextension_types();
+
+#endif // CELLULAR_AUTOMATA_REGISTER_TYPES_H
