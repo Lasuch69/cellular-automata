480bdb038e18cf06e7e4866d19e9466eb6275f03
Lasuch <stylizedstickman@gmail.com>
2023-09-14 18:15:14 +0200

    Change TypedArray to Vector

--- src/cellular_automata.hpp
+++ src/cellular_automata.hpp
@@ -2,6 +2,7 @@
 
 #include <godot_cpp/classes/random_number_generator.hpp>
 #include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/templates/vector.hpp>
 #include <godot_cpp/variant/typed_array.hpp>
 
 using namespace godot;
@@ -14,13 +15,13 @@ class CellularAutomata : public RefCounted {
 	int adjacent_walls_threshold = 5;
 	int nearby_walls_threshold = 2;
 
-	void random_fill(TypedArray<bool> &p_map, int p_width, int p_height, int p_density) const;
-	TypedArray<bool> step(const TypedArray<bool> &p_map, int p_width, int p_height) const;
+	void random_fill(Vector<bool> &p_map, int p_width, int p_height, int p_density) const;
+	Vector<bool> step(const Vector<bool> &p_map, int p_width, int p_height) const;
 
-	bool get_tile_type(const TypedArray<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
+	bool get_tile_type(const Vector<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
 
-	int get_adjacent_walls_count(const TypedArray<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
-	int get_nearby_walls_count(const TypedArray<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
+	int get_adjacent_walls_count(const Vector<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
+	int get_nearby_walls_count(const Vector<bool> &p_map, int p_width, int p_height, int p_x, int p_y) const;
 
 protected:
 	static void _bind_methods();
